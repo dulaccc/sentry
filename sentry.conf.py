6bd702a66ba32294d1478cb0dd1260ffd621d5d4
Pierre Dulac <dulacpier@gmail.com>
2014-11-05 15:44:42 +0100

    Initial commit and config

---
+++ sentry.conf.py
@@ -0,0 +1,44 @@
+from os import environ
+from urlparse import urlparse
+
+import dj_database_url
+
+# default configuration
+from sentry.conf.server import *
+
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost/direct-sentry')
+}
+
+# No trailing slash!
+# The sentry service base url http://sentry.example.com
+SENTRY_URL_PREFIX = environ.get('SENTRY_URL_PREFIX', '')
+
+# Random secret key
+SENTRY_KEY = environ.get('SENTRY_KEY', '')
+
+# Web Service
+SENTRY_WEB_HOST = 'localhost'
+SENTRY_WEB_PORT = 9000
+SENTRY_WEB_OPTIONS = {
+    'workers': 3,
+    'limit_request_line': 0,  # required for raven-js
+}
+
+# Redis backend
+REDIS = urlparse.urlparse(environ.get('REDISCLOUD_URL', 'redis://localhost:6379'))
+SENTRY_REDIS_OPTIONS = {
+    'hosts': {
+        0: {
+            'host': REDIS.hostname,
+            'port': REDIS.port,
+            'password': REDIS.password,
+        }
+    }
+}
+
+# Mailing
+MANDRILL_API_KEY = environ.get('MANDRILL_APIKEY', '')
+EMAIL_BACKEND = 'djrill.mail.backends.djrill.DjrillBackend'
+EMAIL_SUBJECT_PREFIX = '[Sentry] '
